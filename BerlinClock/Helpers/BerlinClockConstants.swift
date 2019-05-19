db66c3d5e1ede8b04f8321eb66203ba916df6d75
Vaibhav <vaibhav5thfeb@gmail.com>
2019-05-19 22:38:15 +0530

    All constants from the project have been separated & isolated in this file

---
+++ BerlinClock/Helpers/BerlinClockConstants.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+struct BerlinClockConstants {
+    
+    public static let PADDING: CGFloat = 10
+    public static let NUMBER_OF_ONEMINUTES_BLOCKS = 4
+    public static let NUMBER_OF_FIVEMINUTES_BLOCKS = 11
+    public static let NUMBER_OF_HOUR_BLOCKS = 4
+    public static let VALUE_OF_MINUTE_BLOCKS:Int = 5
+    public static let VALUE_OF_HOUR_BLOCKS:Int = 5
+    
+}
